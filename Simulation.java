c309b6b9673d134ee86e7f5540393bee7f80952d
Hongxiang Fu <50134879+Hongxiang2023@users.noreply.github.com>
2019-05-01 10:28:15 +0300

    Update Simulation.java

--- Simulation.java
+++ Simulation.java
@@ -1,36 +1,52 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class Simulation extends JPanel {
+
+		
 	private final int width = 1000, height = 700;
-	
-	public Simulation() {
-		BoxLayout boxlayout = new BoxLayout(this, BoxLayout.Y_AXIS);
-		setLayout(boxlayout);
-		JTextArea displayarea = new JTextArea();
-		displayarea.setEditable(false);
+	Image img, smallerimg;
 		
-		JFrame frame = new JFrame();
-		frame.setSize(width, height);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(this);
-		frame.setLocationRelativeTo(null);
-		frame.setResizable(false);
-		frame.setVisible(true);
-		this.setFocusable(true);
 		
-	
-	}
-	
-	public void run() {
+		public Simulation() {
+			BoxLayout boxlayout = new BoxLayout(this, BoxLayout.Y_AXIS);
+			setLayout(boxlayout);
+			JTextArea displayarea = new JTextArea();
+			displayarea.setEditable(false);
+			
+			JFrame frame = new JFrame();
+			frame.setSize(width, height);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(this);
+			frame.setLocationRelativeTo(null);
+			frame.setResizable(false);
+			frame.setVisible(true);
+			this.setFocusable(true);
+			
+			img = Toolkit.getDefaultToolkit().getImage("spaceb.jpg");
+			smallerimg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+			
+		}
 		
-	}
+		public void run() {
+			
+		}
+		
+		public void paint(Graphics g) {
+			
+			g.drawImage(smallerimg,0,0,this);
+		}
 
-	public static void main(String[] args) {
-		Simulation test =  new Simulation();
+		public static void main(String[] args) {
+		
+			Simulation test =  new Simulation();
 
-	}
+		}
 
 }
