1d2f773ef67bc1e1853be47738aae674f0f82f3a
katada7 <50134847+katada7@users.noreply.github.com>
2019-05-06 12:23:30 +0300

    Add files via upload

--- Simulation.java
+++ Simulation.java
@@ -18,7 +18,7 @@ public class Simulation extends JPanel {
 	ArrayList<Entity> entities = new ArrayList<Entity>();
 	
 	int timescale = 1500;
-	
+	double GraphicsScaling = 2e9;
 	
 		public Simulation() {
 			BoxLayout boxlayout = new BoxLayout(this, BoxLayout.Y_AXIS);
@@ -43,10 +43,10 @@ public class Simulation extends JPanel {
 			try {
 				
 				//sun
-				entities.add(new Entity(width*2e10/2, height*2e10/2, 0, 0, 1.989e30, 695510, "Sun.png"));
+				entities.add(new Entity(0, 0, 0, 0, 1.989e30, 695510, "Sun.png"));
 				
 				//earth
-				entities.add(new Entity(width*2e10/2 - 149.6e9, height*2e10, 0, -3e6, 5.972e24, 6371, "Earth.png"));
+				entities.add(new Entity(-149.6e9,0, 0, 3e4, 5.972e24, 6371, "Earth.png"));
 				
 				//moon
 				//entities.add(new Entity(width/3, height/2-30, 2, 0, 7.348e22, 1737.1, "Moon.png"));
@@ -74,18 +74,18 @@ public class Simulation extends JPanel {
 		
 		public void paint(Graphics g) {
 
-			
 			//g.drawImage(smallerimg,0,0,this);
 
 
 			for(Entity pl: entities) {
-				//Image i = pl.img;
-				//g.drawImage(i, dx1, dy1, dx2, dy2, sx1, sy1, sx2, sy2, observer);
+				//this updates the posiion of the planets
+				//the for loop is there to make the simulation go faster
 				for(int i=0;i<timescale; i++)
 				pl.update(entities);
+				
 				g.setColor(Color.RED);
-				g.fillOval((int)((pl.x-pl.rad)/1e10),(int) ((pl.y-pl.rad)/1e10),(int) Math.log(pl.rad), (int) Math.log(pl.rad));
-				System.out.println(pl.x/2e10 +" and "+ pl.y/2e10);
+				
+				g.fillOval((int)((pl.x/2e9)+width/2), (int)((pl.y/2e9)+height/2), (int)(Math.log(pl.rad)), (int)(Math.log(pl.rad)) );
 				
 				
 
